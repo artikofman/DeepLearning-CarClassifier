126e7ca6522820a3a48cb84be70f49eba252d5e3
orik <artikofman@gmail.com>
2020-01-14 00:29:29 +0200

    completion of CNN code, including basic tests. Addign remarks remaining.

--- ConvNetwork.py
+++ ConvNetwork.py
@@ -56,16 +56,27 @@ class ConvNetwork:
         # A matrix (tensor) of real classes. The values in this matrix at runtime correspond to the images held
         # in the matrices above.
         self.__real__ = tf.placeholder(tf.float32, [None, 1])
+
         self._init_conv_layers()
 
-        # TODO complete the remark
-        num_rows_last_conv = image_shape[0] / (2**len(self.__conv_layers_num_kernels__))
-        num_cols_last_conv = image_shape[1] / (2**len(self.__conv_layers_num_kernels__))
+        # Given that invoking self._init_conv_layers declared and created the last convolutional layer, the one held
+        # in self.__conv_pools__[-1], and that each max-pooling layer reduces its input by 2,
+        # the lines following declare and initialize self.__conv_flat_output__ as a flat tensor of
+        # the last convolutional layer. This tensor is the input of the fully-connected step of our CNN.
+        num_rows_last_conv = int(image_shape[0] / (2 ** len(self.__conv_layers_num_kernels__)))  # First dimension of each one of last kernels. Numbers of rows.
+        num_cols_last_conv = int(image_shape[1] / (2 ** len(self.__conv_layers_num_kernels__)))  # Second dimension of each one of last kernels. Numbers of columns.
         num_kernels_last_conv = self.__conv_layers_num_kernels__[-1]
-        self.__conv_flat_output__ = tf.reshape(self.__conv_pools__[-1],
-                                               [-1, num_rows_last_conv * num_cols_last_conv * num_kernels_last_conv])
+        self.__fc_batch_input_num_cols__ = num_rows_last_conv * num_cols_last_conv * num_kernels_last_conv
+        self.__conv_flat_output__ = tf.reshape(self.__conv_pools__[-1], [-1, self.__fc_batch_input_num_cols__])
 
         self._init_fc_layers()
+        self._init_final_layer()
+        self.__train_loss__ = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.__real__,
+                                                                                     logits=self.__final_output__))
+        self.__train_update__ = tf.train.GradientDescentOptimizer(self.__train_rate__).minimize(self.__train_loss__)
+        self.__sess__ = tf.Session()
+        self.__initialized__ = False
+        self.__closed__ = False
 
     def _init_conv_layers(self):
         self.__conv_weights__ = []
@@ -90,7 +101,7 @@ class ConvNetwork:
                              padding=ConvNetwork.CONST_PADDING) + self.__conv_biases__[i])
             self.__conv_activations__.append(tf.nn.relu(self.__conv_outputs__[i]))
             self.__conv_pools__.append(tf.nn.max_pool(self.__conv_activations__[i], ksize=[1, 2, 2, 1],
-                                                     strides=[1, 2, 2, 1], padding=ConvNetwork.CONST_PADDING))
+                                                      strides=[1, 2, 2, 1], padding=ConvNetwork.CONST_PADDING))
 
     def _init_fc_layers(self):
         self.__fc_weights__ = []
@@ -99,11 +110,130 @@ class ConvNetwork:
         self.__fc_activations__ = []
         for (i, num) in enumerate(self.__fc_layers_num_neurons__):
             if i == 0:
-
+                prev_dim = self.__fc_batch_input_num_cols__
+                prev_output = self.__conv_flat_output__
             else:
+                prev_dim = self.__fc_layers_num_neurons__[i - 1]
+                prev_output = self.__fc_activations__[i - 1]
+            self.__fc_weights__.append(tf.Variable(tf.truncated_normal(shape=[prev_dim, num], stddev=0.1)))
+            self.__fc_biases__.append(tf.Variable(tf.truncated_normal(shape=[num], stddev=0.1)))
+            self.__fc_outputs__.append(tf.matmul(prev_output, self.__fc_weights__[i]) + self.__fc_biases__[i])
+            self.__fc_activations__.append(tf.nn.relu(self.__fc_outputs__[i]))
 
+    def _init_final_layer(self):
+        self.__fc_weights__.append(tf.Variable(tf.truncated_normal(shape=[self.__fc_layers_num_neurons__[-1], 1],
+                                                                   stddev=0.1)))
+        self.__fc_biases__.append(tf.Variable(tf.truncated_normal(shape=[1], stddev=0.1)))
+        self.__fc_outputs__.append(tf.matmul(self.__fc_activations__[-1], self.__fc_weights__[-1]) +
+                                   self.__fc_biases__[-1])
+        self.__final_output__ = tf.nn.sigmoid(self.__fc_outputs__[-1])
 
+    def init_net_session(self):
+        if self.__closed__:
+            raise Exception("Network has been closed.")
+        self.__sess__.run(tf.global_variables_initializer())
+        self.__sess__.run(tf.local_variables_initializer())
+        self.__initialized__ = True
 
+    def close(self):
+        if self.__sess__ is not None:
+            self.__sess__.close()
+        self.__closed__ = True
+
+    def run(self, run_type: int):
+        if self.__closed__:
+            raise Exception("Network has been closed.")
+        elif not self.__initialized__:
+            raise Exception("Network session has not been initialized yet. Call 'init_net_session' method.")
+
+        loss_mean = 0
+        loss_queue = []
+        curr_loss = 0
+        distance_tensor = tf.reduce_mean(tf.abs(self.__real__ - self.__final_output__))
+        accuracy_queue = []
+        num_true = 0
+
+        if run_type == ConvNetwork.RUN_TRAIN:
+            print("\nCNN training start. %s" % datetime.now())
+            self.__images_cntrl__.init_train_data()
+            num_iterate = self.__train_num_iterate__
+            next_batch_getter = self.__images_cntrl__.get_next_train_batch
+            fetches = [self.__train_update__, self.__train_loss__, distance_tensor]
+            if num_iterate >= 50:
+                interval_show_status = num_iterate // 50
+            else:
+                interval_show_status = 1
+            kernel = "({0}, {1})".format(str(self.__conv_kernel_dimensions__[0]),
+                                         str(self.__conv_kernel_dimensions__[1]))
+            conv_layers = " -> ".join(map(str, self.__conv_layers_num_kernels__))
+            fc_layers = str(self.__fc_batch_input_num_cols__) + " -> " + \
+                        " -> ".join(map(str, self.__fc_layers_num_neurons__)) + " -> 1"
+            classes_ratio = "{0} vehicles / {1} non-vehicles".format(str(self.__train_classes_ratio__.numerator),
+                                                                     str(self.__train_classes_ratio__.denominator))
+            print("Convolutional layers: each kernel %s, number of kernels %s.\n"
+                  "Fully-connected layers: %s.\n"
+                  "Training rate %f.\n"
+                  "Classes ratio: %s\n"
+                  "Batch size %d. Number of iterations %d.\n"
+                  "Results are being printed every %d steps.\n"
+                  % (kernel, conv_layers, fc_layers, self.__train_rate__, classes_ratio, self.__batch_size__,
+                     num_iterate, interval_show_status))
+        else:
+            print("\nCNN test start. %s\n" % datetime.now())
+            self.__images_cntrl__.init_test_data()
+            num_iterate = (self.__images_cntrl__.num_test // self.__batch_size__) + \
+                          (1 if self.__images_cntrl__.num_test % self.__batch_size__ > 0 else 0)
+            next_batch_getter = self.__images_cntrl__.get_next_test_batch
+            fetches = [self.__final_output__, self.__train_loss__, distance_tensor]
+            interval_show_status = 0
+
+        i = 0
+        has_more_images = True
+        while i < num_iterate and has_more_images:
+            batch_xs, batch_ys = next_batch_getter()
+            if batch_xs is None:
+                has_more_images = False
+            else:
+                _, curr_loss, curr_distance = self.__sess__.run(fetches=fetches,
+                                                                feed_dict={self.__raw_input_batch__: batch_xs,
+                                                                           self.__real__: batch_ys})
+                curr_accuracy = True if curr_distance <= 0.02 else False
+                if run_type == ConvNetwork.RUN_TRAIN:
+                    loss_queue.append(curr_loss)
+                    accuracy_queue.append(curr_accuracy)
+                    if i >= 10:
+                        first_loss = loss_queue.pop(0)
+                        first_accuracy = accuracy_queue.pop(0)
+                        if first_accuracy:
+                            num_true = max(0, num_true - 1)
+                    else:
+                        first_loss = 0
+                    if curr_accuracy:
+                        num_true = min(10, num_true + 1)
+                    loss_mean = loss_mean - (first_loss / 10) + (curr_loss / 10)
+
+                    if i >= 10 and i % interval_show_status == 0:
+                        print("Step %d: loss %0.2f, accuracy %d percents. %s"
+                              % (i, loss_mean, num_true * 10, datetime.now()))
+                else:
+                    loss_mean += (curr_loss / num_iterate)
+                    if curr_accuracy:
+                        num_true += 1
+
+                if isnan(curr_loss):
+                    print("NaN values. Process has been aborted.")
+                    break
+                i += 1
+
+        if not isnan(curr_loss):
+            if run_type == ConvNetwork.RUN_TRAIN:
+                print("\nCNN training end. %s\nFinal loss %0.2f. Final accuracy %d percents.\n"
+                      % (datetime.now(), loss_mean, num_true * 10))
+            else:
+                print("\nCNN test end. %s\nFinal loss %0.2f. Final accuracy %0.2f percents.\n"
+                      % (datetime.now(), loss_mean, 100 * (num_true / num_iterate)))
+
+    """
     def _init_layers(self):
         prev_num = self.__inner_layers_num_neurons__[0]
         self.__weights__ = [tf.Variable(tf.truncated_normal(shape=[self.__num_inputs__, prev_num], stddev=0.1))]
@@ -122,14 +252,16 @@ class ConvNetwork:
             tf.matmul(self.__outputs__[self.__num_inner_layers__ - 1], self.__weights__[self.__num_inner_layers__]) +
             self.__biases__[self.__num_inner_layers__])
         self.__output__ = tf.nn.sigmoid(self.__outputs__[self.__num_inner_layers__])
+    """
 
+"""
 # TEMP
 x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
 W_conv1 = tf.Variable(tf.truncated_normal([5, 5, 1, 32], stddev=0.1))
 b_conv1 = tf.Variable(tf.constant(0.1, shape=[32]))
-x_image = tf.reshape(x, [-1,28,28,1])  # if we had RGB, we would have 3 channels
+x_image = tf.reshape(x, [-1, 28, 28, 1])  # if we had RGB, we would have 3 channels
 
 h_conv1 = tf.nn.relu(tf.nn.conv2d(x_image, W_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
 h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
@@ -140,7 +272,7 @@ b_conv2 = tf.Variable(tf.constant(0.1, shape=[64]))
 h_conv2 = tf.nn.relu(tf.nn.conv2d(h_pool1, W_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2)
 h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
 W_fc1 = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1024], stddev=0.1))
 b_fc1 = tf.Variable(tf.constant(0.1, shape=[1024]))
 
@@ -152,21 +284,21 @@ h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 W_fc2 = tf.Variable(tf.truncated_normal([1024, 10], stddev=0.1))
 b_fc2 = tf.Variable(tf.constant(0.1, shape=[10]))
 
-y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)  # uses moving averages momentum
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
 for i in range(20000):
-  batch = mnist.train.next_batch(50)
-  if i%100 == 0:
-    train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
-    print("step %d, training accuracy %g"%(i, train_accuracy))
-  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    batch = mnist.train.next_batch(50)
+    if i % 100 == 0:
+        train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+        print("step %d, training accuracy %g" % (i, train_accuracy))
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
-
+print("test accuracy %g" % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+"""
--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+from ConvNetwork import ConvNetwork
 from Network import Network
 from LogisticRegression import LogisticRegression
 from fractions import Fraction
@@ -5,6 +6,7 @@ from tkinter import messagebox
 
 
 def main():
+    """
     log_regression = LogisticRegression(vehicles_train_path="C:\\Users\\orik\\DLImages\\vehicles\\Train\\",
                                         non_vehicles_train_path="C:\\Users\\orik\\DLImages\\non-vehicles\\Train\\",
                                         vehicles_test_path="C:\\Users\\orik\\DLImages\\vehicles\\Test\\",
@@ -28,6 +30,18 @@ def main():
     net.run(net.RUN_TRAIN)
     net.run(net.RUN_TEST)
     net.close()
+    """
+
+    cnn = ConvNetwork(vehicles_train_path="C:\\Users\\orik\\DLImages\\vehicles\\Train\\",
+                      non_vehicles_train_path="C:\\Users\\orik\\DLImages\\non-vehicles\\Train\\",
+                      vehicles_test_path="C:\\Users\\orik\\DLImages\\vehicles\\Test\\",
+                      non_vehicles_test_path="C:\\Users\\orik\\DLImages\\non-vehicles\\Test\\",
+                      image_shape=(64, 64), conv_layers_num_kernels=(12, 24), conv_kernel_dimensions=(5, 5),
+                      fc_layers_num_neurons=(1000, 200, 30), batch_size=30, train_classes_ratio=Fraction(2, 1),
+                      train_rate=0.001, train_num_iterate=100000)
+    cnn.init_net_session()
+    cnn.run(cnn.RUN_TRAIN)
+    cnn.close()
 
     messagebox.showinfo(title="Classifier", message="Mission complete")
 
